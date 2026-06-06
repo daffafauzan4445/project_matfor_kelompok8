54a5b95e8e7ee87624bd8c6aea8b12486b82373c
daffafauzan4445 <daffaabil2921@gmail.com>
2026-06-06 22:38:48 +0700

    Refactor data and centroid initialization logic

--- data.py
+++ data.py
@@ -1,45 +1,58 @@
+import math
+
 dataAwal = [
-    ["acha",            [4,4,4], 0],
-    ["Khilda",          [3,4,4], 0],
-    ["Kharisa",         [3,3,4], 0],
-    ["Nadiana",         [4,2,3], 0],
-    ["Faiq",            [5,4,5], 0],
-    ["Khansa",          [4,5,4], 0],
-    ["ripda",           [4,3,3], 0],
-    ["Elbima",          [3,4,3], 0],
-    ["Aqsah",           [5,2,4], 0],
-    ["Dhafin",          [4,3,3], 0],
-    ["Sylvia",          [3,4,3], 0],
-    ["Indah",           [3,4,4], 0],
-    ["Ridwan",          [5,2,2], 0],
-    ["Hasna",           [4,3,3], 0],
-    ["Aflah",           [5,5,5], 0],
-    ["Fakhira",         [5,5,3], 0],
-    ["Arifah",          [4,4,3], 0],
-    ["Almaidah",        [4,4,3], 0],
-    ["Nazhwa",          [4,4,4], 0],
-    ["Lesley",          [2,2,2], 0],
-    ["Afit",            [4,4,4], 0],
-    ["Zidni",           [4,4,4], 0],
-    ["Barra",           [4,4,4], 0],
-    ["Elbima2",         [3,4,3], 0],
-    ["Yahfasyat",       [4,4,4], 0],
-    ["Tuska",           [1,1,5], 0],
-    ["Najib",           [3,3,3], 0],
-    ["Lucky",           [4,4,4], 0],
-    ["Anonim",          [3,4,3], 0],
-    ["Adiba",           [4,4,4], 0],
-    ["Raditya",         [4,5,3], 0],
-    ["RATUW",           [3,4,3], 0],
-    ["Daffa",           [5,5,3], 0],
-]
-
-k = 3
-
-centroid = [
-    [5, 4, 5],
-    [3, 3, 3],
-    [1, 1, 5],
+    ["Nama 1",   [4,4,4], 0],
+    ["Nama 2",   [3,4,4], 0],
+    ["Nama 3",   [3,3,4], 0],
+    ["Nama 4",   [4,2,3], 0],
+    ["Nama 5",   [5,4,5], 0],
+    ["Nama 6",   [4,5,4], 0],
+    ["Nama 7",   [4,3,3], 0],
+    ["Nama 8",   [3,4,3], 0],
+    ["Nama 9",   [5,2,4], 0],
+    ["Nama 10",  [4,3,3], 0],
+    ["Nama 11",  [3,4,3], 0],
+    ["Nama 12",  [3,4,4], 0],
+    ["Nama 13",  [5,2,2], 0],
+    ["Nama 14",  [4,3,3], 0],
+    ["Nama 15",  [5,5,5], 0],
+    ["Nama 16",  [5,5,3], 0],
+    ["Nama 17",  [4,4,3], 0],
+    ["Nama 18",  [4,4,3], 0],
+    ["Nama 19",  [4,4,4], 0],
+    ["Nama 20",  [3,4,5], 0],
+    ["Nama 21",  [4,4,4], 0],
+    ["Nama 22",  [4,4,4], 0],
+    ["Nama 23",  [4,4,4], 0],
+    ["Nama 24",  [3,4,3], 0],
+    ["Nama 25",  [4,4,4], 0],
+    ["Nama 26",  [1,1,5], 0],
+    ["Nama 27",  [3,3,3], 0],
+    ["Nama 28",  [4,4,4], 0],
+    ["Nama 29",  [3,4,3], 0],
+    ["Nama 30",  [4,4,4], 0],
+    ["Nama 31",  [4,5,3], 0],
+    ["Nama 32",  [3,4,3], 0],
+    ["Nama 33",  [5,5,3], 0],
 ]
 
 
+def ruleOfThumb():
+    n = len(dataAwal)
+    return round(math.sqrt(n / 2))
+
+
+def buatCentroid(k):
+    centroids = []
+    seen = []
+    for row in dataAwal:
+        if row[1] not in seen:
+            seen.append(row[1])
+            centroids.append(list(row[1]))
+        if len(centroids) == k:
+            break
+    return centroids
+
+
+k = ruleOfThumb()
+centroid = buatCentroid(k)
